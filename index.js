be32b50cec32394128ec64dc4c9ba11d354b0272
Michael Wheeler <git@michael-wheeler.org>
2016-09-11 20:33:53 +1000

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var mqtt = require('mqtt');
 
 homeduino = require('homeduino')
 Board = homeduino.Board
-board = new Board("serialport", {serialDevice:'/dev/tty.wchusbserial14a20', baudrate: 115200})
+board = new Board("serialport", {serialDevice:'/dev/ttyAMA0', baudrate: 115200})
 
 var client  = mqtt.connect('mqtt://localhost:1883');
 
@@ -13,20 +13,30 @@ client.on('connect', function () {
 board.on("rfReceive", function(event){
   console.log ('received:', event.pulseLengths, event.pulses)
 })
- 
+
 board.on("rf", function(event){
   var topic = event.protocol + "/" + event.values.id;
   console.log(event)
   if (event.values.state != undefined){
-  	console.log(topic + " - " + event.values.state.toString())
-  	client.publish(topic, event.values.state.toString());
+        console.log(topic + " - " + event.values.state.toString())
+        client.publish(topic, event.values.state.toString());
   }
 })
- 
+
 board.connect().then( function() {
   console.log ("board ready")
   board.rfControlStartReceiving(0).then( function(){
     console.log ("receiving...")
+    tempInterval = setInterval(function() {
+      try {
+      board.readDHT(11, 13).then( function(ret) {
+        client.publish("/dht11/temperature", ret.temperature.toString());
+        client.publish("/dht11/humidity", ret.humidity.toString());
+      }
+
+      )
+    } catch (e) {};
+    }, 5000);
   }).done()
 }).done()
 
@@ -36,10 +46,11 @@ client.on('message', function (topic, message) {
   console.log(topic)
   var options = {'state': (message.toString().trim() === "true")}
   var protocol = topic.split("/")[1]
-  options.id = topic.split("/")[2] 
+  options.id = topic.split("/")[2]
   options.unit = topic.split("/")[3]
-  
+
   console.log(options)
   board.rfControlSendMessage(4,3,protocol,options)
   console.log(message.toString());
 });
+
